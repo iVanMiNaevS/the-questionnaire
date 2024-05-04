379f88b410e064bf18c524367d9603c207d7c748
Ivan Minaev <minaev70ivan009@gmail.com>
2024-05-04 22:22:05 +0300

    change name

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-	user: process.env.USER,
-	password: process.env.PASSWORD,
-	host: process.env.HOST,
+	user: process.env.USERDB,
+	password: process.env.PASSWORDDB,
+	host: process.env.HOSTDB,
 	port: process.env.PORTBD,
 	database: process.env.DATABASE,
 });
